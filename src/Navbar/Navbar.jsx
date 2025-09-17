9622e687b57aa369c6f88f677e5b79077fb26350
Abdul Kader <168454554+abdulkader3@users.noreply.github.com>
2025-09-17 14:17:15 +0600

    Add memo functionality to Navbar

--- src/Navbar/Navbar.jsx
+++ src/Navbar/Navbar.jsx
@@ -1,19 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
+import { CgProfile } from "react-icons/cg";
 import { FaBarsStaggered } from "react-icons/fa6";
+import { FiShoppingCart } from "react-icons/fi";
 import { IoSearchOutline } from "react-icons/io5";
+import { LuBoxes } from "react-icons/lu";
+import { MdOutlineManageAccounts, MdOutlineNotificationsNone } from "react-icons/md";
+import { TiShoppingCart } from "react-icons/ti";
+import { VscHeart } from "react-icons/vsc";
 import { Link } from "react-router-dom";
 
 const Navbar = () => {
+  const [memoOpen, setMemoOpen] = useState(false);
   return (
     <>
       <nav>
         <ul className="flex justify-between px-2 items-center py-2  bg-white">
+
           <li>
             <Link to="#">
               {" "}
               <img src="photos/insparetionLogo.webp" alt="logo" />{" "}
             </Link>
           </li>
+
+          {/* Search box */}
           <li>
             <div className="Search relative">
               <input
@@ -26,10 +36,93 @@ const Navbar = () => {
               </button>
             </div>
           </li>
+          {/* Search box */}
+
+          {/* Memo Button */}
           <li>
-            <FaBarsStaggered />
+            <button onClick={() => setMemoOpen((prev) => !prev)}>
+             <FaBarsStaggered /> 
+            </button>
           </li>
+          {/* Memo Button */}
+          
+
         </ul>
+
+        {/* memo content */}
+        <div
+          className={`w-fit h-screen rounded-2xl fixed top-14 right-0 bg-[#eff7ff] py-5 px-10 shadow-[-2px_1px_10px_6px_rgba(0,_0,_0,_0.1)] transition-transform duration-500 ease-in-out z-50
+            ${memoOpen ? "translate-x-0" : "translate-x-full"}`}
+        >
+          <div className="profile-Image flex flex-col items-center">
+            <div className="profilePhotos w-[100px] rounded-full overflow-hidden ">
+              <img className="w-full" src="photos/demoProfileImage.webp" alt="profile" />
+            </div>
+            {/* name id */}
+            <div className="Name-id text-center ">
+              <p className="font-bold text-[21px] " >Name Joni</p>
+              <p className="font-semibold" >ID: 002 21</p>
+            </div>
+            {/* name id */}
+            {/* Others links  */}
+            <div className="Others-Links w-full mt-5 text-center ">
+              {/* ...existing code... */}
+              <div className="LingBox mb-5 text-xl">
+                <div className=" white-line bg-white h-[2px] w-[250px] rounded-full "></div>
+              <div className="login-singUp my-2">
+                <Link className="flex text-[22px] item-center gap-5" to="#" > <CgProfile className="text-[28px] " /> <span>Login/Sing Up</span> </Link>
+              </div>
+              </div>
+              {/* ...existing code... */}
+              <div className="LingBox mb-5 text-xl">
+                <div className=" white-line bg-white h-[2px] w-[250px] rounded-full "></div>
+              <div className="login-singUp my-2">
+                <Link className="flex text-[22px] item-center gap-5" to="#" > <MdOutlineManageAccounts className="text-[28px] " /> <span>My Profile</span> </Link>
+              </div>
+              </div>
+              {/* ...existing code... */}
+              <div className="LingBox mb-5 text-xl">
+                <div className=" white-line bg-white h-[2px] w-[250px] rounded-full "></div>
+              <div className="login-singUp my-2">
+                <Link className="flex text-[22px] item-center gap-5" to="#" > <MdOutlineNotificationsNone className="text-[28px] " /> <span>Notification</span> </Link>
+              </div>
+              </div>
+              {/* ...existing code... */}
+              <div className="LingBox mb-5 text-xl">
+                <div className=" white-line bg-white h-[3px] w-[250px] rounded-full "></div>
+              <div className="login-singUp my-2">
+                <Link className="flex text-[22px] item-center gap-5" to="#" > <LuBoxes className="text-[28px] " /> <span>All products</span> </Link>
+              </div>
+              </div>
+              {/* ...existing code... */}
+              <div className="LingBox mb-5 text-xl">
+                <div className=" white-line bg-white h-[2px] w-[250px] rounded-full "></div>
+              <div className="login-singUp my-2">
+                <Link className="flex text-[22px] item-center gap-5" to="#" > <TiShoppingCart className="text-[28px] "  /> <span>My Order</span> </Link>
+              </div>
+              </div>
+              {/* ...existing code... */}
+              <div className="LingBox mb-5 text-xl">
+                <div className=" white-line bg-white h-[3px] w-[250px] rounded-full "></div>
+              <div className="login-singUp my-2">
+                <Link className="flex text-[22px] item-center gap-5" to="#" > <FiShoppingCart className="text-[28px] " /> <span>cart</span> </Link>
+              </div>
+              </div>
+              {/* ...existing code... */}
+              <div className="LingBox mb-5 text-xl">
+                <div className=" white-line bg-white h-[2px] w-[250px] rounded-full "></div>
+              <div className="login-singUp my-2">
+                <Link className="flex text-[22px] item-center gap-5" to="#" > <VscHeart className="text-[28px] " /> <span>Wishlist</span> </Link>
+              </div>
+              <div className=" white-line bg-white h-[2px] w-[250px] rounded-full "></div>
+              </div>
+              {/* ...existing code... */}
+            </div>
+            {/* Others links  */}
+          </div>
+        </div>
+        {/* memo content */}
+
       </nav>
     </>
   );
