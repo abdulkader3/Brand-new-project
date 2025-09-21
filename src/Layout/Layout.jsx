d253e76d8519d4ed8d06355255ac1e0bc5a5e004
Abdul Kader <168454554+abdulkader3@users.noreply.github.com>
2025-09-21 19:34:30 +0600

    Implement responsive design enhancements for ProductDetails and Layout components

--- src/Layout/Layout.jsx
+++ src/Layout/Layout.jsx
@@ -6,18 +6,18 @@ import ManuBar from '../Page/ManuBar.jsx'
 const Layout = () => {
   return (
     <>
-  <div className="flex flex-col min-h-screen w-full">
+  <div className="flex flex-col min-h-screen w-full bg-gray-50 lg:bg-white">
         {/* Navbar always at the top */}
         <div className="sticky top-0 z-50">
           <Navbar />
         </div>
 
-        <div className="flex-1 flex flex-col pb-10 md:pb-20">
+        <div className="flex-1 flex flex-col pb-16 md:pb-20 lg:pb-8 xl:pb-12">
           <Outlet />
         </div>
 
-        {/* MenuBar always at the bottom */}
-        <div className="sticky bottom-0 z-50">
+        {/* MenuBar always at the bottom - hidden on large desktop */}
+        <div className="sticky bottom-0 z-50 lg:hidden">
           <ManuBar/>
         </div>
       </div>
